64e444534e1b4d092ca2495a06145fcfc3c10485
Tim Whiteaker <whiteaker@utexas.edu>
2019-04-05 16:52:11 -0500

    Show results in an HTML table

--- zotero.js
+++ zotero.js
@@ -3,11 +3,16 @@
 "use strict";
 
 var ZOTERO_CONFIG = {
-   "zotId": "2055673", // ID of group or user library to search in Zotero
+   "zotId": "2211939", // ID of group or user library to search in Zotero, e.g., 2211939, 2055673
    "zotIdType": "group", // group or user
-   "collectionKey": "", // Key of collection within library to search, or "" if no collection.
-   "limit": 10, // Max number of results to retrieve per page
+   "collectionKey": "", // Key of collection within library to search, e.g., KHTHLKB5, or "" if no collection
+   "filterTags": "", // For filtering results by tag(s), e.g., "&tag=LTER-Funded".  See examples at https://www.zotero.org/support/dev/web_api/v3/basics
    "resultsElementId": "searchResults", // Element to contain results
+   "includeCols": ["Year", "Type", "ShowTags"], // Array of columns to include in the output table, other than Citation. The full set is ["Year", "Type", "ShowTags"]
+   "showTags": ["Foundational", "LTER-Funded", "LTER-Enabled"], // Include a column showing this tag if present for each item
+   "showTagColName": "Relationship", // Name for the column in HTML table under which the showTags will appear
+   "style": "", // Bibliography display style, e.g., apa. Leave blank for default which is chicago-note-bibliography.
+   "limit": 50, // Max number of results to retrieve per page
    "urlElementId": "searchUrl", // Element to display search URL
    "countElementId": "resultCount", // Element showing number of results
    "pagesTopElementId": "paginationTop", // Element to display result page links above results
@@ -31,8 +36,120 @@ function getParameterByName(name, url) {
 
 // Parse Zotero search results into HTML
 function parseZoteroResults(resultText) {
+   function parseYear(text) {
+      function isYear(yearText) {
+         // Assumes year is between 1000 and current year + 5
+         try {
+            var yr = parseInt(yearText);
+            var maxAllowable = (new Date().getFullYear()) + 5;
+            return (String(yr) === yearText && yr >= 1000 && yr <= maxAllowable)
+         } catch (err) {
+            return false;
+         }
+      }
+
+      if (text) {
+         if (isYear(text.slice(0, 4))) {
+            return text.slice(0, 4);
+         } else if (isYear(text.slice(-4))) {
+            return text.slice(-4);
+         } else {
+            return text;
+         }
+      } else {
+         return "";
+      }
+   }
+
+   function parseType(text) {
+      switch (text) {
+         case "artwork":
+            return "Artwork";
+         case "audioRecording":
+            return "Audio Recording";
+         case "bill":
+            return "Bill";
+         case "blogPost":
+            return "Blog Post";
+         case "book":
+            return "Book";
+         case "bookSection":
+            return "Book Section";
+         case "case":
+            return "Case";
+         case "computerProgram":
+            return "Computer Program";
+         case "conferencePaper":
+            return "In Proceedings";
+         case "dictionaryEntry":
+            return "Dictionary Entry";
+         case "document":
+            return "Document";
+         case "email":
+            return "Email";
+         case "encyclopediaArticle":
+            return "Encyclopedia Article";
+         case "film":
+            return "Film";
+         case "forumPost":
+            return "Forum Post";
+         case "hearing":
+            return "Hearing";
+         case "instantMessage":
+            return "Instant Message";
+         case "interview":
+            return "Interview";
+         case "journalArticle":
+            return "Journal Article";
+         case "letter":
+            return "Letter";
+         case "magazineArticle":
+            return "Magazine Article";
+         case "manuscript":
+            return "Manuscript";
+         case "map":
+            return "Map";
+         case "newspaperArticle":
+            return "Newspaper Article";
+         case "patent":
+            return "Patent";
+         case "podcast":
+            return "Podcast";
+         case "presentation":
+            return "Presentation";
+         case "radioBroadcast":
+            return "Radio Broadcast";
+         case "report":
+            return "Report";
+         case "statute":
+            return "Statute";
+         case "thesis":
+            return "Thesis";
+         case "tvBroadcast":
+            return "TV Broadcast";
+         case "videoRecording":
+            return "Video Recording";
+         case "webpage":
+            return "Web Page";
+         case "note":
+            return "Note";
+         default:
+            return "Other"
+      }
+   }
+
+   function parseShowTags(tags) {
+      function objToString(tagObj) {
+         return tagObj["tag"];
+      }
+      var tagArray = tags.map(objToString);
+      var matches = tagArray.filter(function (n) {
+         return ZOTERO_CONFIG["showTags"].indexOf(n) !== -1;
+      });
+      return matches.join(", ");
+   }
+
    var results = JSON.parse(resultText);
-   var html = [];
    var sortDiv = document.getElementById(ZOTERO_CONFIG["sortDiv"]);
    if (sortDiv) {
       if (results.length)
@@ -40,15 +157,52 @@ function parseZoteroResults(resultText) {
       else
          sortDiv.style.display = "none";
    }
-   for (var i = 0; i < results.length; i++) {
-      var result = results[i];
-      html.push(result["bib"]);
-   }
-   if (html.length) {
-      return html.join("<br>");
-   } else {
+   if (!results.length) {
       return "<p>Your search returned no results.</p>";
    }
+
+   var header = '<div class="table-responsive-xl"><table class="table table-striped table-sm">';
+   var showYear = (ZOTERO_CONFIG["includeCols"].indexOf("Year") !== -1);
+   var showType = (ZOTERO_CONFIG["includeCols"].indexOf("Type") !== -1);
+   var showTags = (ZOTERO_CONFIG["includeCols"].indexOf("ShowTags") !== -1);
+   if (ZOTERO_CONFIG["includeCols"].length > 0) {
+      header += "<thead><tr>";
+      if (showYear) {
+         header += "<th>Year</th>";
+      }
+      header += "<th>Citation</th>";
+      if (showType) {
+         header += "<th>Type</th>";
+      }
+      if (showTags) {
+         header += "<th>" + ZOTERO_CONFIG["showTagColName"] + "</th>";
+      }
+      header += "</tr></thead><tbody>";
+   }
+
+   var rows = [header];
+
+   for (var i = 0; i < results.length; i++) {
+      var result = results[i];
+      var year = parseYear(result["data"]["date"]);
+      var itemType = parseType(result["data"]["itemType"]);
+      var tagsToShow = parseShowTags(result["data"]["tags"]);
+      var row = "<tr>";
+      if (showYear) {
+         row += "<td>" + year + "</td>";
+      }
+      row += "<td>" + result["bib"] + "</td>";
+      if (showType) {
+         row += "<td>" + itemType + "</td>";
+      }
+      if (showTags) {
+         row += "<td>" + tagsToShow + "</td>";
+      }
+      row += "</tr>"
+      rows.push(row);
+   }
+   rows.push("</tbody></table></div>");
+   return rows.join("");
 }
 
 
@@ -113,10 +267,11 @@ function searchZotero(query, itemType, sort, start) {
    var zotId = (ZOTERO_CONFIG["zotIdType"] === "group") ? "groups/" + ZOTERO_CONFIG["zotId"] : "users/" + ZOTERO_CONFIG["zotId"];
    var collection = (ZOTERO_CONFIG["collectionKey"] === "") ? "/" : "/collections/" + ZOTERO_CONFIG["collectionKey"] + "/";
    var base = "https://api.zotero.org/" + zotId + collection + "/items?v=3&include=bib,data";
+   var style = (ZOTERO_CONFIG["style"] === "") ? "" : "&style=" + ZOTERO_CONFIG["style"];
    var params = "&q=" + encodeURI(query) + "&itemType=" + itemType +
-      "&sort=" + sort + "&start=" + start;
+      "&sort=" + sort + "&start=" + start + ZOTERO_CONFIG["filterTags"];
    var limit = "&limit=" + ZOTERO_CONFIG["limit"];
-   var url = base + params + limit;
+   var url = base + params + style + limit;
    showUrl(url);
    showLoading(true);
    makeCorsRequest(url, successCallback, errorCallback);
@@ -191,10 +346,11 @@ function initForm(formId, expanded) {
 
 
 // Selects the desired value in the Select control. If value is not in the
-// control, then first index is used. Returns actual selected value.
+// control, then first index is used. Returns actual selected value.  If
+// control is not present, input value is returned.
 function setSelectValue(elId, desiredValue) {
    var el = document.getElementById(elId);
-   if (!el || !el.length) return null;
+   if (!el || !el.length) return desiredValue;
    var result = el[0].value;
    for (var i = 0; i < el.length; i++) {
       if (desiredValue === el[i].value) {
@@ -210,10 +366,10 @@ function setSelectValue(elId, desiredValue) {
 // When the window loads, read query parameters and perform search
 window.onload = function () {
    var query = getParameterByName("q") || "";
-   var itemTypeParam = getParameterByName("itemType");
+   var itemTypeParam = getParameterByName("itemType") || "-attachment";
    var expanded = Boolean(getParameterByName("expanded"));
    var pageStart = getParameterByName("start") || 0;
-   var sortParam = getParameterByName("sort");
+   var sortParam = getParameterByName("sort") || "date";
 
    document.forms.zoteroSearchForm.q.value = query;
    var itemType = setSelectValue("itemType", itemTypeParam);
